fb4ba7b30fa348de5b60a38ce17907041abc5d55
Akindele <akindeleapex@gmail.com>
2023-10-22 21:55:47 +0100

    pushed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const MONGO_URL = "mongodb+srv://Apex:apex@cluster0.suj6osk.mongodb.net/food-app";
 export const APP_SECRET = "apexvendorSecret";
 export const PORT = process.env.PORT || 3000;
+// test
