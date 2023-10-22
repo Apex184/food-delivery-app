14aae38df272b774a03bd67eac52de601f2d8c7b
Akindele <akindeleapex@gmail.com>
2023-10-22 22:38:08 +0100

    fixing

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_SECRET = exports.MONGO_URL = void 0;
+exports.PORT = exports.APP_SECRET = exports.MONGO_URL = void 0;
 exports.MONGO_URL = "mongodb+srv://Apex:apex@cluster0.suj6osk.mongodb.net/food-app";
 exports.APP_SECRET = "apexvendorSecret";
+exports.PORT = process.env.PORT || 3000;
+// test
 //# sourceMappingURL=index.js.map
--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ExpressApp_1 = __importDefault(require("./services/ExpressApp"));
 const Database_1 = __importDefault(require("./services/Database"));
+const config_1 = require("./config");
 const startServer = async () => {
     const app = (0, express_1.default)();
     await (0, Database_1.default)();
     await (0, ExpressApp_1.default)(app);
-    app.listen(3000, () => {
-        console.log("Server listening on port 3000");
+    app.listen(config_1.PORT, () => {
+        console.log(`Server listening on port ${config_1.PORT}`);
     });
 };
 startServer();
